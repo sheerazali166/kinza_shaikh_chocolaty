f5714ad83276fe6f29148dcd5ccc0fb648914833
Software Scientist <sheerazali656@gmail.com>
2022-09-20 01:24:43 +0500

    ❤️❤️❤️ Kinza Shaikh is so so much chocolaty ❤️❤️❤️

---
+++ life.py
@@ -0,0 +1,10 @@
+import math
+
+theAnswer = 42
+
+def qoute_marvin():
+	print('I\'ve calculated your chance of survival, but I don\'t think you\'ll like it.')
+
+
+def square_root(x):
+	return math.sqrt(x)
